1f5cd258dc495b074d926725428c1ae1de0408d1
Gabe <gtorion97@gmail.com>
2021-07-08 06:43:15 -0500

    create account server connection comp

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,37 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
+
+// post = save new user into the database
+router.route("/users").post((request, response) => {
+    console.log("request received from the frontend")
+    const newUser = new User({
+        firstName: request.body.user.firstName,
+        lastName: request.body.user.lastName,
+        userName: request.body.user.userName,
+        password: request.body.user.password,
+        belief: request.body.user.belief,
+        sex: request.body.user.sex,
+        reasonsToJoin: request.body.user.reasonsToJoin,
+        email: request.body.user.email,
+        phoneNum: request.body.user.phoneNum
+    });
+
+    response.json({
+        status: "backend successfully received your request",
+        receivedData: request.body.user
+    });
+
+    newUser.save();
+})
+
+// get = get the user account when the user signs in
+router.route("/users").get((request, response) => {
+    User.find()
+        .then(users => response.json(users))
+})
+
 
 
 
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
-const { urlencoded } = require('express');
 const mongoose = require('mongoose');
 const express = require('express');
-const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express();
 const dbconnection = "mongodb+srv://gtorio:simba1997@clustercv.blvqa.mongodb.net/CVBlog"
+
+app.use(cors())
+
+app.use(express.json());
 
 mongoose.connect(dbconnection, {
     useUnifiedTopology: true,
@@ -14,52 +17,7 @@ mongoose.connect(dbconnection, {
     console.log("connection to mongodb database is successful!")
 });
 
-const userSchema = {
-    id: String,
-    firstName: String,
-    lastName: String,
-    userName: String,
-    password: String,
-    belief: String,
-    sex: String,
-    reasonsToJoin: String,
-    email: String,
-    phoneNum: String
-}
-
-const User = mongoose.model("users", userSchema)
-
-
-app.use(express.json());
-
-app.get("/users", (request, response) => {
-    response.json("wazzzz up")
-})
-
-
-
-// get the new user account info
-app.post("/users", (request, response) => {
-    console.log("request received from the frontend")
-    const newUser = new User({
-        firstName: request.body.user.firstName,
-        lastName: request.body.user.lastName,
-        userName: request.body.user.userName,
-        password: request.body.user.password,
-        belief: request.body.user.belief,
-        sex: request.body.user.sex,
-        reasonsToJoin: request.body.user.reasonsToJoin,
-        email: request.body.user.email,
-        phoneNum: request.body.user.phoneNum
-    });
-
-    response.json({
-        status: "backend successfully received your request",
-        receivedData: request.body.user
-    });
-
-    newUser.save();
-})
+app.use("/", require("./routes/user"))
 
 app.listen(3005, () => {
     console.log('server started on 3005')
